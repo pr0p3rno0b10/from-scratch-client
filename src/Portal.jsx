ab7677395eb317dc8c7c4577731c4da60aa86779
pr0p3rno0b <jacknemtsov@MacBookAir.lan>
2025-08-20 15:27:56 +0100

    Working on Portal

--- src/Portal.jsx
+++ src/Portal.jsx
@@ -9,22 +9,24 @@ const Portal = () => {
 		<div>
 			<NavigationBar/>
 			<div className="container">
-				<div className="introduction-box">
-					<div className="introduction-box-left-side">
-						<div className="header">Coding Classes for Kids</div>
-						<div className="paragraph-text">1:1 coding classes for kids aged 7-18 tailored to your needs</div>
-						<div className="first-class-button">Get your first class free!</div>
-					</div>
-					<div className="introduction-box-right-side">
-						<div className="graphic-image"></div>
+				<div className="introduction-box-container">
+					<div className="split">
+						<div className="introduction-box-left-side">
+							<div className="header">Coding Classes for Kids/Adults/Elderly</div>
+							<div className="paragraph-text">1:1 coding classes for kids aged 7-18 tailored to your needs</div>
+							<div className="first-class-button">Get your first class free!</div>
+						</div>
+						<div className="introduction-box-right-side">
+							<div className="graphic-image"></div>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div className="pricing-box">
+				<div className="pricing-box-container">
 				
-			</div>
-			<div className="roadmap-box">
-				
+				</div>
+				<div className="roadmap-box-container">
+					
+				</div>
 			</div>
 		</div>
 	);
