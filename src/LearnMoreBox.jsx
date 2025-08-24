3098905c2437d815cdeb9fea667d09f5d23bd27e
pr0p3rno0b <jnemtsov10@gmail.com>
2025-08-24 15:26:52 +0100

    added addition fundamental LearnMoreBox code

--- src/LearnMoreBox.jsx
+++ src/LearnMoreBox.jsx
@@ -1,12 +1,18 @@
 import "./index.sass";
 
-const LearnMoreBox = () => {
+import { Link } from 'react-router-dom';
+
+
+const LearnMoreBox = ({ currentPage }) => {
 	return (
 		<div className="learn-more-box">
 			<div className="learn-more-box__header">
-				Explore <span className="learn-more-box__header__highlight">From Scratch</span>'s website to learn more!
+				Keep on look around <span className="learn-more-box__header__highlight">From Scratch</span>'s website to learn more!
 			</div>
 			<div className="learn-more-box__redirection-buttons-container">
+				{ currentPage !== "home" && <Link to="/" className="learn-more-box__redirection-buttons-container__redirection-button">Home Page</Link> }
+				{ currentPage !== "roadmap" && <Link to="/roadmap" className="learn-more-box__redirection-buttons-container__redirection-button">Roadmap</Link> }
+				{ currentPage !== "pricing" && <Link to="/pricing" className="learn-more-box__redirection-buttons-container__redirection-button">Pricing</Link>}
 			</div>
 		</div>
 	);
